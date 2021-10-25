f3eeabe5f437452d56c55837497c1ff2738c4ea9
赖大铖 <490315348@qq.com>
2021-10-25 16:33:29 +0800

    修改重新命名仓库出入库及时率

--- 仓库出入库及时率.py
+++ 仓库出入库及时率.py
@@ -6,6 +6,7 @@ from datetime import timedelta
 import openpyxl
 
 from numpy import datetime64
+from openpyxl import load_workbook
 
 now = datetime.date.today()
 
@@ -38,15 +39,22 @@ Purchase_in_data.loc[Purchase_in_data["审批延时"] <= 72, "单据状态"] = "
 # 材料出库及时率
 Material_out_data = pd.read_excel(f"./KPI/SCM/WM/材料出库单列表-{last_month_start}-{last_month_end}.XLSX",
                                   usecols=['出库单号', '材料编码', '物料描述', '审核时间', '制单时间'],
-                                  converters={'材料编码': str})
+                                  converters={'材料编码': str, '出库单号': str})
 
 Material_out_data = Material_out_data.dropna(axis=0, how='any')  # 去除所有nan的列
-Material_out_data = Material_out_data.drop_duplicates()  # 去重
+
 Material_out_data['审批延时'] = ((Material_out_data['审核时间'] - Material_out_data['制单时间']) / pd.Timedelta(1, 'H')).astype(
     int)
 # 将天数转化为小时数
 Material_out_data.loc[Material_out_data["审批延时"] > 72, "单据状态"] = "超时"  # 计算出来的审批延时大于72为超时
 Material_out_data.loc[Material_out_data["审批延时"] <= 72, "单据状态"] = "正常"  # 小于等于72为正常
+
+# Purchase_in_data = Purchase_in_data.drop_duplicates()  # 去重
+Material_out_data = Material_out_data.drop_duplicates()  # 去重
 # 保存
 Purchase_in_data.to_excel('./KPI/SCM/WM/仓库出入库及时率.xlsx', sheet_name="仓库入库及时率", index=False)
-Material_out_data.to_excel('./KPI/SCM/WM/仓库出入库及时率.xlsx', sheet_name="仓库出库及时率", index=False)
+book = load_workbook('./KPI/SCM/WM/仓库出入库及时率.xlsx')
+writer = pd.ExcelWriter("./KPI/SCM/WM/仓库出入库及时率.xlsx", engine='openpyxl')
+writer.book = book
+Material_out_data.to_excel(writer, "仓库出库及时率", index=False)
+writer.save()
