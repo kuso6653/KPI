8bb7a7446a21851b8aa4b1a40bb5581669aa545c
赖大铖 <490315348@qq.com>
2022-03-23 09:11:05 +0800

    新增 SCM_OP_采购订单转换及时率_Daily 生产性物料采购订单执行效率（是否及时下单）： a)	逻辑沿用U8采购订单转换及时率； b)	报表周期：调整为日报；

--- SCM_OP_采购订单转换及时率_Daily.py
+++ SCM_OP_采购订单转换及时率_Daily.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import calendar
 import datetime
-from datetime import timedelta
+from datetime import timedelta, datetime
 from numpy import datetime64
 from openpyxl import load_workbook
 
@@ -12,134 +12,108 @@ pd.set_option('display.max_columns', None)
 
 class OrderConversion:
     def __init__(self):
+        self.YesMRPData = None
         self.func = Func
         self.ThisMonthStart, self.ThisMonthEnd, self.LastMonthEnd, self.LastMonthStart = self.func.GetDate()
         self.path = Func.Path()
         self.MRPScreenList = []  # 筛选合并的mrp数据
         self.MRPNewDataList = []  # 本月所有的mrp数据
         self.GroupMRPList = []  # 分组后的mrp数据
-
+        self.today = datetime.today()  # datetime类型当前日期
+        yesterday = self.today + timedelta(days=-3)  # 减去三天
+        while 1:  # 获取前三天MRP数据，没有则继续往前推一天，直到有为止
+            try:
+                self.YesMRPData = pd.read_excel(f"{self.path}/DATA/SCM/OP/MRP计划维护--全部{str(yesterday)[:10]}.XLSX",
+                                                usecols=['物料编码', '物料名称', '需求跟踪号', '需求跟踪行号', '物料属性', '是否客供料', '开工日期'],
+                                                converters={'物料编码': int, '需求跟踪行号': int, '开工日期': str}
+                                                )
+                break
+            except Exception as e:
+                print(e)
+                yesterday = yesterday + timedelta(days=-1)
+        self.MRPData = pd.read_excel(f"{self.path}/DATA/SCM/OP/MRP计划维护--全部{str(self.today)[:10]}.XLSX",
+                                     usecols=['物料编码', '物料名称', '需求跟踪号', '需求跟踪行号', '物料属性', '是否客供料', '开工日期'],
+                                     converters={'物料编码': int, '需求跟踪行号': int, '开工日期': str}
+                                     )
         self.ThisMonthStart = str(self.ThisMonthStart).split(" ")[0]
         self.LastMonthStart = str(self.LastMonthStart).split(" ")[0]
         self.ThisMonthEnd = str(self.ThisMonthEnd).split(" ")[0]
         self.this_month_check = self.ThisMonthStart
 
-        OtherThisMonthStart = str(self.ThisMonthStart).split(" ")[0].replace("-", "")
-        OtherLastMonthStart = str(self.LastMonthStart).split(" ")[0].replace("-", "")
-        OtherThisMonthEnd = str(self.ThisMonthEnd).split(" ")[0].replace("-", "")
-        self.po_data = pd.read_excel(f"{self.path}/DATA/SCM/OP/采购订单列表.XLSX",
-                                     usecols=['请购单号', '存货编码', '存货名称', '行号', '行关闭人'],
-                                     converters={'请购单号': str, '订单编号': int, '行号': int, '存货编码': int}
-                                     )
-        self.po_data = self.po_data.loc[self.po_data["行关闭人"].isnull()]
-        self.pr_data = pd.read_excel(f"{self.path}/DATA/SCM/OP/请购单列表.XLSX",
-                                     usecols=['单据号', '存货编码', '存货名称', '行号', '行关闭人', '建议订货日期', '审核时间'],
-                                     converters={'单据号': str, '行号': int, '存货编码': int, '审核时间': datetime64,
-                                                 '建议订货日期': datetime64})
-        self.pr_data = self.pr_data.loc[self.pr_data["行关闭人"].isnull()]
-        self.pr_notapprove = self.pr_data.loc[self.pr_data["审核时间"].isnull()]
-        self.pr_data = self.pr_data.loc[self.pr_data["审核时间"].isnotnull()]
-        self.pr_notapprove['建议订货日期'] = datetime.date.today()
+        self.POData = pd.read_excel(f"{self.path}/DATA/SCM/OP/采购订单列表.XLSX",
+                                    usecols=['订单编号', '实际到货日期', '行号', '制单时间'],
+                                    converters={'订单编号': str, '实际到货日期': datetime64, '制单时间': datetime64, '行号': str}
+                                    )
+        self.POData = self.POData.rename(columns={'行号': '采购订单行号', '订单编号': '采购订单号', '制单时间': '采购订单制单时间'})
+
+        self.PRProcessData = pd.read_excel(f"{self.path}/DATA/SCM/OP/请购执行进度表.XLSX",
+                                           usecols=[1, 6, 7, 8, 9, 10, 14, 15, 20, 23], header=4,
+                                           names=["请购单号", "请购单行号", "存货编码", "存货名称", "规格型号", "数量", "采购订单号", "采购订单行号",
+                                                  "计划到货日期", "采购员"],
+                                           converters={'请购单号': str, '请购单行号': str, '存货编码': str, '数量': float,
+                                                       '采购订单号': str, '采购订单行号': str, '计划到货日期': datetime64})
+
+        self.PRData = pd.read_excel(f"{self.path}/DATA/SCM/OP/请购单列表.XLSX",
+                                    usecols=['单据号', '行号', '行关闭人', '建议订货日期', '审核时间', '制单时间', '存货编码', '存货名称', '规格型号',
+                                             '数量'],
+                                    converters={'单据号': str, '行号': str, '审核时间': datetime64, '制单时间': datetime64,
+                                                '建议订货日期': datetime64, '存货编码': str, '数量': float})
+        self.PRData = self.PRData.rename(columns={'行号': '请购单行号', '单据号': '请购单号', '制单时间': '请购单制单时间', '审核时间': '请购单审核时间'})
+
+        self.PRData = self.PRData.loc[self.PRData["行关闭人"].isnull()]  # 筛选 行关闭人 为空的行
+        self.PRApproveNotTime = self.PRData.loc[self.PRData["请购单审核时间"].isnull()]  # 筛选 审核时间 为空的行
+        self.PRApproveNotTime = self.PRApproveNotTime.loc[  # 筛选 建议订货日期 小于 当天 的行
+            self.PRApproveNotTime['建议订货日期'] < datetime64(str(self.today)[:10])]
+
+        self.PRData = self.PRData.drop(['存货编码', '存货名称', '规格型号', '数量'], axis=1)
 
     def mkdir(self, path):
         self.func.mkdir(path)
 
-    def ContrastData(self, BaseData, NewBase):
-        BaseData = BaseData.dropna(subset=['物料编码'])  # 去除nan的列
-        NewBase = NewBase.dropna(subset=['物料编码'])  # 去除nan的列
-        out_data = pd.merge(BaseData, NewBase.drop(labels=['抓取时间', '是否客供料'], axis=1),
-                            on=['物料编码', '物料名称', '需求跟踪号', '需求跟踪行号', '物料属性'])
-        self.MRPScreenList.append(out_data)
-
-    def GetOrderConversion(self):
-        # 获取截取这个月份、年、上个月
-
-        this_month = self.ThisMonthEnd.split("-")[1]
-        year = self.ThisMonthEnd.split("-")[0]
-        last_month = self.LastMonthStart.split("-")[1]
-
-        last_work_days = self.func.WorkDays(year, last_month)  # 获取上个月工作日
-        this_work_days = self.func.WorkDays(year, this_month)  # 获取这个月工作日
-        work_days = []  # 设置到上月3天
-        work_days.extend(last_work_days[-3:])
-        work_days.extend(this_work_days)  # 将上个月最后三天和这个月工作日相合并
-        work_days = self.func.ReformDays(work_days)  # 改造
-
-        BaseDataList = []
-        flag = 0
-        for work_day in work_days:
-            if flag < 3:
-                try:
-                    base_data = pd.read_excel(f"{self.path}/DATA/SCM/OP/MRP计划维护--全部{year}-{last_month}-{work_day}.XLSX",
-                                              usecols=['物料编码', '物料名称', '需求跟踪号', '需求跟踪行号', '物料属性', '是否客供料'],
-                                              converters={'物料编码': int, '需求跟踪行号': int}
-                                              )
-                    base_data = base_data.loc[base_data["物料属性"] == "采购"]
-                    base_data = base_data.loc[base_data["是否客供料"].isnull()]
-                    catch_data = datetime64(f"{year}-{last_month}-{work_day}")
-                    base_data['抓取时间'] = catch_data
-                    BaseDataList.append(base_data)
-                    flag = flag + 1
-                    continue
-                except:
-                    flag = flag + 1
-                    continue
-            else:
-                try:
-                    new_data = pd.read_excel(f"{self.path}/DATA/SCM/OP/MRP计划维护--全部{year}-{this_month}-{work_day}.XLSX",
-                                             usecols=['物料编码', '物料名称', '需求跟踪号', '需求跟踪行号', '物料属性', '是否客供料'],
-                                             converters={'物料编码': int, '需求跟踪行号': int}
-                                             )
-                    self.MRPNewDataList.append(new_data)
-                except:
-                    continue
-                catch_data = datetime64(f"{year}-{this_month}-{work_day}")
-                new_data['抓取时间'] = catch_data
-                new_data = new_data.loc[new_data["物料属性"] == "采购"]
-                new_data = new_data.loc[new_data["是否客供料"].isnull()]
-                BaseDataList.append(new_data)  # 新添加新的base
-                self.ContrastData(BaseDataList[0], new_data)  # 合并检查是否存在一样的
-                del (BaseDataList[0])  # 删除第一个base
-        all_mrp = pd.concat(self.MRPScreenList, axis=0, ignore_index=True)
-        for name1, group in all_mrp.groupby(['物料编码', '需求跟踪号', '需求跟踪行号']):
-            group = pd.DataFrame(group)  # 新建pandas
-            group = group.sort_values(by='抓取时间', ascending=True)  # 升序排序
-            max_data_list = group.head(1)  # 取最早的抓取时间，就是排序后的第一列
-            self.GroupMRPList.append(max_data_list)  # 加入list
-            flag = flag + 1
-
-        self.GroupMRPData = pd.concat(self.GroupMRPList, axis=0, ignore_index=True)
-
-    def ThisMonthNotConverted(self):  # 当月未转换MRP清单
-        self.pr_data = self.pr_data.rename(columns={'单据号': '请购单号'})  # 修改单据号为请购单号
-        self.pr_data = self.pr_data.dropna(subset=['存货编码'])  # 去除nan的列
-        self.po_data = self.po_data.dropna(subset=['存货编码'])  # 去除nan的列
-
-        self.pr_data.reset_index()
-        #  当月大于2天的未转换MRP数据筛选
-        MRPNow_data = self.GroupMRPData[self.GroupMRPData['抓取时间'] >= datetime64(self.this_month_check)]
-        self.mkdir(self.path + '/RESULT/SCM/OP')
-        self.pr_data.to_excel(f'{self.path}/RESULT/SCM/OP/采购订单转换及时率.xlsx', sheet_name="当月未转换采购订单的请购单", index=False)
+    def ADDSheet(self, df, name):
         book = load_workbook(f'{self.path}/RESULT/SCM/OP/采购订单转换及时率.xlsx')
         writer = pd.ExcelWriter(f"{self.path}/RESULT/SCM/OP/采购订单转换及时率.xlsx", engine='openpyxl')
         writer.book = book
-        MRPNow_data.to_excel(writer, "当月未转换采购订单的MRP计划订单", index=False)
+        df.to_excel(writer, f"{name}", index=False)
         writer.save()
 
-    def HistoryNotConverted(self):  # 历史未转换MRP清单
-        self.mkdir(self.path + '/RESULT/SCM/OP')
-        #  小于当月的历史未转换MRP数据筛选
-        MRPHistory_data = self.GroupMRPData[self.GroupMRPData['抓取时间'] < datetime64(self.this_month_check)]
-        book = load_workbook(f'{self.path}/RESULT/SCM/OP/采购订单转换及时率.xlsx')
-        writer = pd.ExcelWriter(f"{self.path}/RESULT/SCM/OP/采购订单转换及时率.xlsx", engine='openpyxl')
-        writer.book = book
-        MRPHistory_data.to_excel(writer, "历史未转换采购订单的MRP计划订单", index=False)
-        writer.save()
+    def PRProcessConvert(self):  #
+        df = pd.merge(self.PRProcessData, self.POData, on=['采购订单号', '采购订单行号'], how='left')
+        # df.to_excel('./demo2.xlsx', index=False)
+        df2 = pd.merge(df, self.PRData, on=['请购单行号', '请购单号'], how='left')
+        Approve = df2.loc[df2["请购单审核时间"].notnull()]  # 筛选 请购单审核时间 不为空的值
+        ApproveNotTime = df2.loc[df2["请购单审核时间"].isnull()]  # 筛选 请购单审核时间 为空的值
+        df3 = Approve.loc[Approve["采购订单号"].notnull()]  # 筛选 采购订单号 不为空的值
+        df4 = Approve.loc[Approve["采购订单号"].isnull()]  # 筛选 采购订单号 为空的值
 
+        df3['转化延时'] = ((df3['采购订单制单时间'] - df3['请购单审核时间']) / pd.Timedelta(1, 'H')).astype(int)
+        df3.loc[df3["转化延时"] > 48, "单据状态"] = "超时"
+        df3.loc[df3["转化延时"] <= 48, "单据状态"] = "正常"
+
+        df4 = df4.loc[df4['建议订货日期'] < datetime64(str(self.today)[:10])]
+
+        df3.to_excel(f'{self.path}/RESULT/SCM/OP/采购订单转换及时率.xlsx', sheet_name="三天内未及时转化请购单", index=False)
+        self.ADDSheet(df4, '历史未转化请购单')
+        self.ADDSheet(self.PRApproveNotTime, '历史未审核请购单')
+        self.ADDSheet(ApproveNotTime, '未完成采购并且已被关闭请购单')
+
+    def GetOrderConversion(self):  # 三天未转化MRP
+        self.YesMRPData = self.YesMRPData.loc[self.YesMRPData["物料属性"] == "采购"]
+        self.YesMRPData = self.YesMRPData.loc[self.YesMRPData["是否客供料"].isnull()]
+
+        self.MRPData = self.MRPData.loc[self.MRPData["物料属性"] == "采购"]
+        self.MRPData = self.MRPData.loc[self.MRPData["是否客供料"].isnull()]
+        df = pd.merge(
+            self.MRPData.drop(labels=['物料名称', '物料属性', '是否客供料'],
+                              axis=1), self.YesMRPData, on=['物料编码', '需求跟踪号', '需求跟踪行号', '开工日期'])
+        df = df.loc[df["需求跟踪号"] == "XS20211223001"]
+        df2 = df.loc[df["需求跟踪号"] != "XS20211223001"]
+        self.ADDSheet(df, '挤出项目未转换MRP清单')
+        self.ADDSheet(df2, '三天内未转化MRP清单')
     def run(self):
+        self.PRProcessConvert()
         self.GetOrderConversion()
-        self.ThisMonthNotConverted()
-        self.HistoryNotConverted()
+        # self.HistoryNotConverted()
 
 
 if __name__ == '__main__':
--- demo.py
+++ demo.py
@@ -1,12 +1,8 @@
-import calendar
-from datetime import timedelta
-import datetime
-now = datetime.date.today()
-# 获取当月首尾日期
-ThisMonthStart = datetime.datetime(now.year, now.month, 1)
-ThisMonthEnd = datetime.datetime(now.year, now.month, calendar.monthrange(now.year, now.month)[1])
-# 获取上月首尾日期
-LastMonthEnd = ThisMonthStart - timedelta(days=1)
-LastMonthStart = datetime.datetime(LastMonthEnd.year, LastMonthEnd.month, 1)
-ThisMonthStart = str(ThisMonthStart).split(" ")[0]
-print(ThisMonthStart, ThisMonthEnd, LastMonthEnd, LastMonthStart)
+import Func
+import pandas as pd
+from datetime import datetime, date, timedelta
+import re
+
+str1 = '物料名称1'
+str2 = re.findall(r'[\u4e00-\u9fa5]{4}[0-9]', str1)
+print(str2)
